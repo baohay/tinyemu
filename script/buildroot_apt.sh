17b2109767433e904c37ffeccddb1cd0fc1c1150
baohay <vsttk01@protonmail.com>
2021-12-08 05:31:57 +0000

    xft

--- script/buildroot_apt.sh
+++ script/buildroot_apt.sh
@@ -1,3 +1,4 @@
 #! /bin/sh
-sudo apt update
-sudo apt install -y xsel rsync cpio binutils patch bzip2 python binutils-riscv64-linux-gnu gcc-multilib g++-multilib
+sudo dpkg --add-architecture riscv64
+sudo apt-get update
+sudo apt install -y xsel rsync cpio binutils patch bzip2 python binutils-riscv64-linux-gnu gcc-multilib g++-multilib libncurses5-dev libc6-i386
